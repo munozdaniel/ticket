9bee69a123c89fd60d9be44f130759a00d4e10dc
Munoz Daniel <munozda87@hotmail.com>
2015-07-28 21:44:30 -0300

    Haberes: action upload, guardando archivos

--- app/controllers/HaberesController.php
+++ app/controllers/HaberesController.php
@@ -7,6 +7,21 @@ class HaberesController extends \Phalcon\Mvc\Controller
     {
 
     }
+    public function uploadAction()
+    {
+        //comprueba si hay archivos por subir
+        if ($this->request->hasFiles() == true)
+        {
+            // Print the real file names and sizes
+            foreach ($this->request->getUploadedFiles() as $file) {
 
+                //Print file details
+                echo $file->getName(), " ", $file->getSize(), "\n";
+
+                //guardamos dentro del directorio img
+                $file->moveTo('temp/' . $file->getName());
+            }
+        }
+    }
 }
 
